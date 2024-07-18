c6839c94e17c51a9bb0a67c238dee432ce119bcb
Barry Kramer <bkramer@snelstart.nl>
2024-07-18 08:35:19 +0200

    baseLine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TableComponent } from './components/table/table.component';
+import { PersonsComponent } from './components/persons/persons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TableComponent,
+    PersonsComponent
   ],
   imports: [
     BrowserModule,
