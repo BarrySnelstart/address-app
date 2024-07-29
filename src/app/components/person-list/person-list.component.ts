0b0dafaed29a67a6ef2e76b4209031e7ff5f9654
Barry Kramer <bkramer@snelstart.nl>
2024-07-29 13:31:31 +0200

    Add routing

--- src/app/components/person-list/person-list.component.ts
+++ src/app/components/person-list/person-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Person } from 'src/app/Person';
+import { Person } from 'src/app/models/Person';
 import { PERSONS } from 'src/app/mock-persons';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -11,9 +13,17 @@ import { PERSONS } from 'src/app/mock-persons';
 export class PersonListComponent implements OnInit{
   persons: Person[] = PERSONS;
 
-  constructor () { }
+  constructor (private router: Router) { }
 
   ngOnInit(): void {}
 
+  personDetails(id : number): void{
+    this.router.navigate(['details']);
+    //this.barrydetails.push({city: 'Den helder', id: 1, firstName: 'barry', lastName: 'kramer'})
+    // this.barrydetails.city = "Den Helder";
+    // this.barrydetails.firstName = "barry";
+    // this.barrydetails.lastName = "Kramer";
+    //console.log(this.barrydetails);
+ }
 
 }
--- src/app/mock-persons.ts
+++ src/app/mock-persons.ts
@@ -1,4 +1,4 @@
-import { Person } from './Person';
+import { Person } from '../app/models/Person';
 
 export const PERSONS: Person []= [
     {
@@ -65,71 +65,5 @@ export const PERSONS: Person []= [
       "id": 13,
       "firstName": "John",
       "lastName": "Hernandez"
-    },    {
-      "id": 1,
-      "firstName": "Barry",
-      "lastName": "Kramer"
     },
-    {
-      "id": 2,
-      "firstName": "Bob",
-      "lastName": "Builder"
-    },
-    {
-      "id": 3,
-      "firstName": "Han",
-      "lastName": "Solo"
-    },
-    {
-      "id": 4,
-      "firstName": "Alice",
-      "lastName": "Smith"
-    },
-    {
-      "id": 5,
-      "firstName": "David",
-      "lastName": "Johnson"
-    },
-    {
-      "id": 6,
-      "firstName": "Sarah",
-      "lastName": "Williams"
-    },
-    {
-      "id": 7,
-      "firstName": "James",
-      "lastName": "Brown"
-    },
-    {
-      "id": 8,
-      "firstName": "Patricia",
-      "lastName": "Jones"
-    },
-    {
-      "id": 9,
-      "firstName": "Michael",
-      "lastName": "Miller"
-    },
-    {
-      "id": 10,
-      "firstName": "Linda",
-      "lastName": "Davis"
-    },
-    {
-      "id": 11,
-      "firstName": "Robert",
-      "lastName": "Garcia"
-    },
-    {
-      "id": 12,
-      "firstName": "Elizabeth",
-      "lastName": "Martinez"
-    },
-    {
-      "id": 13,
-      "firstName": "John",
-      "lastName": "Hernandez"
-    }
-
-
   ];
