a0ef7b03e691b8321f3eaa91515be202b2621aca
Barry Kramer <bkramer@snelstart.nl>
2024-07-30 14:29:54 +0200

    Added Deleteoption

--- src/app/components/person-details/person-details.component.ts
+++ src/app/components/person-details/person-details.component.ts
@@ -1,29 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { PERSONS } from 'src/app/mock-persons';
+import { PersondetailService } from 'src/app/services/persondetail.service';
 import { Person } from 'src/app/models/Person';
-
 
 @Component({
   selector: 'app-person-details',
   templateUrl: './person-details.component.html',
-  styleUrls: ['./person-details.component.scss']
+  styleUrls: ['./person-details.component.scss'],
 })
-export class PersonDetailsComponent implements OnInit{
-  persons: Person[] = PERSONS;
+export class PersonDetailsComponent implements OnInit {
+@Output() onDeleteTask: EventEmitter<Person> = new EventEmitter();
+
+  persons: Person[] = [];
   public id: number | undefined;
-  constructor (private router: Router, private activatedRoute: ActivatedRoute) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute,  private persondetailService: PersondetailService) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.id = parseInt(params.get('id') ?? '', 10);
+      this.persondetailService.getPersons().subscribe((persons) => (this.persons = persons));
+    });
+  }
+  onDelete (person: any){
+this.onDeleteTask.emit(person);
   }
 
-ngOnInit(): void {
-  this.activatedRoute.paramMap.subscribe(params => {
-    // let id = params.get('id');
-    this.id = parseInt(params.get('id')??'',10) ;
-  });
+  getBack() {
+    this.router.navigate(['list']);
+  }
+  deleteTask(person: Person){
+    this.persondetailService
+    .deletePerson(person)
+    .subscribe(() => (this.persons = this.persons.filter(t => t.id !== person.id)));
+  }
 }
-
-getBack () {
-  this.router.navigate(['list']);
-}
-}
-
