7fcfd2fbdb96d28b32e65bef81b30f3abe938d2d
Barry Kramer <bkramer@snelstart.nl>
2024-08-05 13:29:53 +0200

    Added Bootstrap

--- src/app/models/Person.ts
+++ src/app/models/Person.ts
@@ -1,8 +1,8 @@
-export interface Person  {
-  id?: number;
-  firstName: string;
-  lastName: string;
-  nameSufix?:string;
+export class Person {
+  private _id?: number;
+  public firstName: string;
+  public lastName: string;
+  nameSufix?: string;
   streetName: string;
   houseNumber: number;
   houseNumberSufix?: string;
@@ -11,4 +11,29 @@ export interface Person  {
   country: string;
   telephoneNumber: string;
   emailAdress: string;
+
+  constructor(obj: any) {
+    this._id = obj.id;
+    this.firstName = obj.firstName;
+    this.lastName = obj.lastName;
+    this.nameSufix = obj.nameSufix;
+    this.streetName = obj.streetName;
+    this.houseNumber = obj.houseNumber;
+    this.houseNumberSufix = obj.houseNumberSufix;
+    this.zipCode = obj.zipCode;
+    this.city = obj.city;
+    this.country = obj.country;
+    this.telephoneNumber = obj.telephoneNumber;
+    this.emailAdress = obj.emailAdress;
+  }
+  get id(): number | undefined {
+    return this._id;
+  }
+  
+  get fullName(): string {
+    if (this.nameSufix) {
+      return `${this.firstName} ${this.nameSufix} ${this.lastName}`;
+    }
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
